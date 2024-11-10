45d5b3a6d5866cead2b079a610e2bbdebca11759
Alex-777-90 <alxnvn@yahoo.com.br>
2024-11-10 16:42:19 -0300

    ajustando o doenv

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const viewsRouter = require('./router/viewsRouter');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
