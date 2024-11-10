585f0b93d9274b197790acd56922f61b50f13e5d
Alex-777-90 <alxnvn@yahoo.com.br>
2024-11-10 16:08:44 -0300

    ajuste para deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const viewsRouter = require('./router/viewsRouter');
-require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- router/viewsRouter.js
+++ router/viewsRouter.js
@@ -7,17 +7,17 @@ const router = express.Router();
 
 // Rota para a página inicial
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../views', 'index.html'));
 });
 
 // Rota para a página de login
 router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'login.html'));
+    res.sendFile(path.resolve(__dirname, '../views', 'login.html'));
 });
 
 // Rota para a página de administração
 router.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'admin.html'));
+    res.sendFile(path.resolve(__dirname, '../views', 'admin.html'));
 });
 
 
